9cf4996aca8d39f5400a378dd8850ca637d90c46
Jayson De los Reyes <jisundr_@Jaysons-MacBook-Pro.local>
2023-04-05 19:02:07 +0800

    checkpoint: section 2.13

--- remix.config.js
+++ remix.config.js
@@ -14,8 +14,12 @@ module.exports = {
     // v2_errorBoundary: true,
     // v2_meta: true,
     // v2_normalizeFormMethod: true,
+    // v2_routeConvention: true,
     // makes the warning go away in v1.15
-    v2_routeConvention: true,
+    v2_errorBoundary: false,
+    v2_meta: false,
+    v2_normalizeFormMethod: false,
+    v2_routeConvention: false,
   },
   routes(defineRoutes) {
     // uses the v1 convention, works in v1.15+ and v2
